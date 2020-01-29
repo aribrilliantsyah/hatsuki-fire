087763bf88c9943fa29be24d17b71334b2f52e8b
aribrilliantsyah <ariardiansyah101@gmail.com>
2020-01-29 16:33:39 +0700

    [UPDATE]

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 3000
 const v1 = require('./src/routes');
 
 app.use('/v1', v1);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
